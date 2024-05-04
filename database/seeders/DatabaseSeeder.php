65fb18bb2f303ad68e13f16878aeafc666a4e11c
gts.senna <gts.senna@gmail.com>
2024-05-04 12:30:22 -0300

    FEAT: add seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Profile;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Profile::insert([
+            [
+                'name' => 'Admin User',
+                'description' => 'Usuário com todos os privilégios de gerenciamento'
+            ],
+            [
+                'name' => 'Client User',
+                'email' => 'Usuário com privilégios comuns',
+            ]
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::insert([
+            [
+                'name' => 'Administrador Master',
+                'email' => 'admin@mail.com',
+                'password' => Hash::make('1234admin'),
+                'profile_id' => Profile::where('name', 'Admin User')->first()->id
+
+            ],
+            [
+                'name' => 'João',
+                'email' => 'joao@mailcom',
+                'password' => Hash::make('1234'),
+                'profile_id' => Profile::where('name', 'Client User')->first()->id
+            ]
         ]);
     }
 }
