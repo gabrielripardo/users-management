0246f78db7001a659b42d3877144e8783550739d
gts.senna <gts.senna@gmail.com>
2024-05-04 12:29:55 -0300

    FEAT: add profile files

--- database/migrations/2024_05_04_021921_alter_users_table.php
+++ database/migrations/2024_05_04_021921_alter_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone');
+            $table->string('phone')->nullable();
             $table->unsignedBigInteger('profile_id');
             $table->foreign('profile_id')->references('id')->on('profiles');
         });
