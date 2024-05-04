440b51f92132fbd26f70b2cff77101501ec941f2
gts.senna <gts.senna@gmail.com>
2024-05-04 12:31:16 -0300

    FEAT: add route api

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\UsersController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('users', [UsersController::class, "index"]);
+Route::post('users', [UsersController::class, "store"]);
+Route::put('users/{id}', [UsersController::class, "update"]);
+Route::delete('users/{id}', [UsersController::class, "destroy"]);
+
